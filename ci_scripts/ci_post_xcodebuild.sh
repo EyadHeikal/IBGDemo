ca53596438639e1e163f5268602adc087d54ea80
Eyad Heikal <eyadheikal@gmail.com>
2023-03-07 17:46:46 +0200

    Update Script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,8 +12,8 @@ ls ${DWARF_DSYM_FOLDER_PATH}
 echo "Before Find"
 echo "PWD:- ${PWD}"
 
-find "$DWARF_DSYM_FOLDER_PATH"  -type f -name "${CI_PRODUCT}*.dSYM"
-DWARF_DSYM_FILE_NAME=$(find .  -type f -name "${CI_PRODUCT}*.dSYM" | head -1)
+find "${DWARF_DSYM_FOLDER_PATH}" -name "${CI_PRODUCT}*.dSYM"
+DWARF_DSYM_FILE_NAME=$(find "${DWARF_DSYM_FOLDER_PATH}" -name "${CI_PRODUCT}*.dSYM" | head -1)
 echo "DWARF_DSYM_FILE_NAME:- ${DWARF_DSYM_FILE_NAME}"
 
 echo "CI_PRODUCT:- ${CI_PRODUCT}"
