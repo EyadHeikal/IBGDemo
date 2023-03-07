458792892e8f831c3856bcf4b5ff1075fe2e3c5b
Eyad Heikal <eyadheikal@gmail.com>
2023-03-07 11:20:02 +0200

    Use Derived Data Path

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
 #exit(1)
+
+# Type a script or drag a script file from your workspace to insert its path.
+#- -- SCRIPT BEGIN - --
+# SKIP_SIMULATOR_BUILDS=1
+  SCRIPT_SRC=$(find "$CI_DERIVED_DATA_PATH" -name 'Instabug_dsym_upload.sh' | head -1)
+  if [ ! "${SCRIPT_SRC}" ]; then
+    echo "Instabug: err: script not found. Make sure that you're including InstabugResources.bundle in your project directory"
+    exit 1
+  fi
+    APP_TOKEN="d824b0efb5573e97ac7c03fb03d2d0a3"
+  source "${SCRIPT_SRC}"
+#- -- SCRIPT END - --
+
