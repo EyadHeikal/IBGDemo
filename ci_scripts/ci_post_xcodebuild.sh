415206eeb7aa96da4f9c5c4a352bf28cbf0ae4c4
Eyad Heikal <eyadheikal@gmail.com>
2023-03-08 07:56:19 +0200

    Change

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,10 +4,9 @@
 # SKIP_SIMULATOR_BUILDS=1
 SCRIPT_SRC=$(find "$CI_DERIVED_DATA_PATH" -name 'Instabug_dsym_upload.sh' | head -1)
 
-#DWARF_DSYM_FOLDER_PATH="$CI_ARCHIVE_PATH/dSYMs"
+DWARF_DSYM_FOLDER_PATH="$CI_ARCHIVE_PATH/dSYMs"
 #DWARF_DSYM_FILE_NAME="${CI_PRODUCT}.app.dSYM"
-DSYM_PATH="$CI_ARCHIVE_PATH/dSYMs/${CI_PRODUCT}.app.dSYM"
-#DWARF_DSYM_FILE_NAME="Instabug.framework.dSYM"
+DWARF_DSYM_FILE_NAME="Instabug.framework.dSYM"
 
 if [ ! "${SCRIPT_SRC}" ]; then
 echo "Instabug: err: script not found. Make sure that you're including InstabugResources.bundle in your project directory"
